9562c8c8ae200758aa9f91d065991ed91212bd11
Godfather <yjc19979401754@126.com>
2022-07-27 22:02:45 +0800

    second commit

--- src/main/java/com/yjc/git/GitTest.java
+++ src/main/java/com/yjc/git/GitTest.java
@@ -1,8 +1,15 @@
 package com.yjc.git;
 
-/**
- * @author shkstart
- * @create 2022-07-27 21:54
- */
+///**
+// * @author shkstart
+// * @create 2022-07-27 21:54
+// */
 public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("你好世界！11111");
+        System.out.println("你好世界！22222");
+        System.out.println("你好世界！33333");
+        System.out.println("你好世界！44444");
+        System.out.println("你好世界！55555");
+    }
 }
