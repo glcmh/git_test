5e5760edfe4ecca50d38ea28c785a66e900f5bc3
glcmh <105921935+glcmh@users.noreply.github.com>
2022-07-27 22:50:27 +0800

    pull commit

--- src/main/java/com/yjc/git/GitTest.java
+++ src/main/java/com/yjc/git/GitTest.java
@@ -14,5 +14,6 @@ public class GitTest {
         System.out.println("你好世界！master test");
         System.out.println("你好世界！hot-fix test");
         System.out.println("push test");
+        System.out.println("pull test");
     }
 }
